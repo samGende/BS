c4809f83baca23206d349b03c07dcc8597408d86
TyGross <tygross025@gmail.com>
2022-06-20 22:38:19 +0200

    fixed mp3a

--- vorgabe-A5/mp3a.c
+++ vorgabe-A5/mp3a.c
@@ -15,16 +15,11 @@ void idTagFile(const char *fileName,char *comment)
 		return;
 	}
 
-
 	long size = fileInfo.st_size;
-	if(S_ISREG(fileInfo.st_mode) != 0){
-		printf("is regular file\n");
-	}else {
+	if(S_ISREG(fileInfo.st_mode) == 0){
 		printf("Invalid File type %s\n", fileName);
 		return;
 	}
-	printf("%li\n", size);
-
 
 
 	if (!strncmp(".", fileName, 2) || !strncmp("..", fileName, 3))
@@ -40,7 +35,7 @@ void idTagFile(const char *fileName,char *comment)
 
 	//open file
 	FILE* file;
-	file = fopen(fileName, 'r');
+	file = fopen(fileName, "r");
 	if(file == NULL){
 		perror("Fehler mit fopen");
 		return;
@@ -56,9 +51,8 @@ void idTagFile(const char *fileName,char *comment)
 		return;
 	}
 
-
 	char buffer[128];
-	if(fread(buffer, 1, 128, file) != 128){
+	if(fread(buffer, 1, 128, file) != ID3_SIZE){
 		//Fehler, denn nicht alle Bytes werden gelesen.
 		printf("Error beim lesen\n");
 
@@ -67,8 +61,6 @@ void idTagFile(const char *fileName,char *comment)
 		}
 		return;
 	}
-
-	printf("%s\n", buffer);
 
 	if(strncmp(buffer, "TAG",3) != 0){
 		printf("TAG not found in %s\n", fileName);
@@ -79,50 +71,18 @@ void idTagFile(const char *fileName,char *comment)
 		return;
 	}
 
-	struct mp3file mp3;
+	//Speichern buffer in mp3/////
+	struct mp3file *mp3;
+	mp3 = bytesToIdTag(buffer);
 
-	int i = 3;
+	printTag(mp3);
 
-	while (i < 33){
-		printf("%c\n", buffer[i]);
-		mp3.titel[i] = buffer[i];
-		mp3.titel[i] ='a';
-		printf("%c\n", mp3.titel[i]);
-		i++;
-	}
-
-	while (i < 63){
-		mp3.interpret[i] = buffer[i];
-		i++;
-	}
-
-	while (i < 93){
-		mp3.album[i] = buffer[i];
-		i++;
-	}
-
-	while (i < 97){
-		mp3.jahr[i] = buffer[i];
-		i++;
-	}
-
-	while (i < 127){
-		mp3.kommentar[i] = buffer[i];
-		i++;
-	}
-
-	mp3.genre = buffer[i];
-
-	printTag(&mp3);
-
-
-	
+	//Datei und Speicher frei geben
+	free(mp3);
 	if(fclose(file) != 0){
 		perror("Error mit fclose");
 		return;
 	}
-
-	/* HIER MUESST IHR EUREN CODE EINFUEGEN */
 }
 
 /* Die Informationen im Puffer, auf den buffer verweist, *
@@ -130,7 +90,60 @@ void idTagFile(const char *fileName,char *comment)
 struct mp3file* bytesToIdTag(char *buffer)
 {
 	/* HIER MUESST IHR EUREN CODE EINFUEGEN */
-	return NULL;
+	struct mp3file *mp3;
+	mp3 = malloc(sizeof(struct mp3file));
+	if (mp3 == NULL){
+		//malloc Fehler
+		printf("malloc Fehler\n");
+		return NULL;
+	}	
+
+	int i = 3;
+	int j = 0;
+
+	//title 3-32
+	while (i < 33){
+		mp3->titel[j] = buffer[i];
+		i++;
+		j++;
+	}
+
+	//interpret 33-62
+	j = 0;
+	while (i < 63){
+		mp3->interpret[j] = buffer[i];
+		i++;
+		j++;
+	}
+
+	//album 63-92
+	j = 0;
+	while (i < 93){
+		mp3->album[j] = buffer[i];
+		i++;
+		j++;
+	}
+
+	//jahr 93-96
+	j = 0;
+	while (i < 97){
+		mp3->jahr[j] = buffer[i];
+		i++;
+		j++;
+	}
+
+	//kommentar 97-126
+	j = 0;
+	while (i < 127){
+		mp3->kommentar[j] = buffer[i];
+		i++;
+		j++;
+	}
+	
+	//genere 127-128
+	mp3->genre = buffer[i];
+
+	return mp3;
 }
 /*				       *
  * Es werden die in dem struct mp3file *
@@ -138,10 +151,11 @@ struct mp3file* bytesToIdTag(char *buffer)
  *				       */
 void printTag(struct mp3file *mp3)
 {
-	/* HIER MUESST IHR EUREN CODE EINFUEGEN */
-	printf("Titel  %s\n", mp3->titel);
-	printf("Album  %s\n", mp3->album);
-	printf("Kommentar  %s\n", mp3->kommentar);
-	printf("interpret  %s\n", mp3->interpret);
+	printf("Titel: %s\n", mp3->titel);
+	printf("Album: %s\n", mp3->album);
+	printf("Kommentar: %s\n", mp3->kommentar);
+	printf("Jahr: %s\n", mp3->jahr);
+	printf("interpret: %s\n", mp3->interpret);
+	printf("Genre: %s\n", translateGenre(mp3->genre));
 
 }
